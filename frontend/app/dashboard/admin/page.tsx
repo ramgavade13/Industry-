7509d58e3b1d1d847339b4b48e30a326778dcfd3
Ram Vithoba Gavade <ramgavade4@gmail.com>
2026-09-11 18:30:50 +0530

    Update page.tsx

--- frontend/app/dashboard/admin/page.tsx
+++ frontend/app/dashboard/admin/page.tsx
@@ -1,4 +1,4 @@
-import DashboardLayout from "@/components/DashboardLayoutV2";
+import DashboardLayout from "@/components/DashboardLayout";
 import KPICard from "@/components/KPICard";
 import { systemStats, systemUsers } from "@/data/mockData";
 
